05d47bf9681d309ded2e4b354526a5b0d49d3cb6
zaqwer101 <platonov256@gmail.com>
2018-07-07 20:15:14 +0800

    Добавил сборку в PDF по томам

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from lxml import html
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
 
 manga_name = "guran-buru"
 
@@ -25,6 +27,67 @@ def getMangaUrls(list_of_urls):
             str = ""
 
     return a
+
+
+def fill_page_with_image(path, canvas):
+    """
+    Given the path to an image and a reportlab canvas, fill the current page
+    with the image.
+
+    This function takes into consideration EXIF orientation information (making
+    it compatible with photos taken from iOS devices).
+
+    This function makes use of ``canvas.setPageRotation()`` and
+    ``canvas.setPageSize()`` which will affect subsequent pages, so be sure to
+    reset them to appropriate values after calling this function.
+
+    :param   path: filesystem path to an image
+    :param canvas: ``reportlab.canvas.Canvas`` object
+    """
+    from PIL import Image
+
+    page_width, page_height = canvas._pagesize
+
+    image = Image.open(path)
+    image_width, image_height = image.size
+
+    orientation = 1
+
+    # These are the possible values for the Orientation EXIF attribute:
+    ORIENTATIONS = {
+        1: "Horizontal (normal)",
+        2: "Mirrored horizontal",
+        3: "Rotated 180",
+        4: "Mirrored vertical",
+        5: "Mirrored horizontal then rotated 90 CCW",
+        6: "Rotated 90 CW",
+        7: "Mirrored horizontal then rotated 90 CW",
+        8: "Rotated 90 CCW",
+    }
+    draw_width, draw_height = page_width, page_height
+    if orientation == 1:
+        canvas.setPageRotation(0)
+    elif orientation == 3:
+        canvas.setPageRotation(180)
+    elif orientation == 6:
+        image_width, image_height = image_height, image_width
+        draw_width, draw_height = page_height, page_width
+        canvas.setPageRotation(90)
+    elif orientation == 8:
+        image_width, image_height = image_height, image_width
+        draw_width, draw_height = page_height, page_width
+        canvas.setPageRotation(270)
+    else:
+        raise ValueError("Unsupported image orientation '%s'."
+                         % ORIENTATIONS[orientation])
+
+    if image_width > image_height:
+        page_width, page_height = page_height, page_width  # flip width/height
+        draw_width, draw_height = draw_height, draw_width
+        canvas.setPageSize((page_width, page_height))
+
+    canvas.drawImage(path, 0, 0, width=draw_width, height=draw_height,
+                     preserveAspectRatio=True)
 
 
 def downloadManga(manga_name):
@@ -75,4 +138,17 @@ def downloadManga(manga_name):
     return imagefile_names
 
 
+def createPDF(manga_name):
+    volume_number = 1
+    for volume_dir in os.listdir(manga_name):
+        pdf = Canvas(manga_name + "_volume_" + str(volume_number) + ".pdf")
+        for image_file in os.listdir(manga_name + "/" + volume_dir):
+            file_path = manga_name + "/" + volume_dir + "/" + image_file
+            fill_page_with_image(file_path, pdf)
+            pdf.showPage()
+        volume_number += 1
+        pdf.save()
+
+
 print(downloadManga("guran-buru"))
+createPDF("guran-buru")
